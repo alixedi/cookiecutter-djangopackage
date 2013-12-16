5268020365543f5d745ffe9b85dc9e5583c6fcbd
Ali Zaidi <alixedi@bitswits.com>
2013-12-17 02:53:07 +0500

    Updated according to the new contexted hooks in cookiecutter

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,9 @@
 #! /usr/bin/python
+import os
 import json
 from subprocess import call
 from os.path import abspath, dirname, join
 
 basepath = abspath(dirname(__file__))
-f = open(join(basepath, '../cookiecutter.json'))
-conf = json.load(f)
-path = (conf['repo_name'], conf['app_name'])
+path = (os.environ['repo_name'], os.environ['app_name'])
 call(['ln', '-s', '../%s/%s' % path, '../%s/demo/%s' % path])
